dc6d04be08a00e47050135764b5cdf492f548bb9
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-03-20 20:46:04 +0200

    Add mapDispatchToProps (object)

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -3,17 +3,17 @@ import { connect } from 'react-redux';
 import {increment, decrement, setStep} from '../../store/slices/counterSlice';
 
 const Counter = (props) => {
-    const {count, step, incrementCb, decrementCb, setStepCb} = props;
+    const {count, step, increment, decrement, setStep} = props;
     console.log(props)
 
     return (
         <div>
             <p>Count: {count}</p>
             <label>
-                Step: <input type="number" value={step} onChange={setStepCb} />
+                Step: <input type="number" value={step} onChange={({target: {value}}) => setStep(value)} />
             </label>
-            <button onClick={incrementCb}>Increment</button>
-            <button onClick={decrementCb}>Decrement</button>
+            <button onClick={() => increment()}>Increment</button>
+            <button onClick={() => decrement()}>Decrement</button>
         </div>
     );
 }
@@ -25,12 +25,20 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
-        incrementCb: () => dispatch(increment()),
-        decrementCb: () => dispatch(decrement()),
-        setStepCb: ({target: {value}}) => dispatch(setStep(value))
-    }
+// v1
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         incrementCb: () => dispatch(increment()),
+//         decrementCb: () => dispatch(decrement()),
+//         setStepCb: ({target: {value}}) => dispatch(setStep(value))
+//     }
+// }
+
+//v2
+const mapDispatchToProps = {
+    increment,
+    decrement,
+    setStep
 }
 
 // const withState = connect(mapStateToProps);
