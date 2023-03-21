a95c671458edd4b598134d6c40e5d0e4bbbad68b
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-03-21 20:01:52 +0200

    Add theme slice

---
+++ src/store/slices/themeSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+import CONSTANTS from "../../constants";
+const { THEMES } = CONSTANTS;
+
+const initialState = THEMES.DARK;
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        setTheme: (state) => (state === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK),
+    }
+})
+
+export const { setTheme }= themeSlice.actions;
+export default themeSlice.reducer;
