def99225f34768ad0e94562b52c3333d72557511
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-03-21 20:23:05 +0200

    Add theme display for Header + theme switching

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import cx from 'classnames';
+import CONSTANTS from '../../constants';
+import { setTheme } from '../../store/slices/themeSlice';
 import styles from './Header.module.scss';
 
-const Header = () => {
+const { THEMES } = CONSTANTS;
+
+const Header = ({theme, setTheme, language}) => {
+    const className = cx(styles.header, {
+        [styles.darkTheme]: theme === THEMES.DARK,
+        [styles.lightTheme]: theme === THEMES.LIGHT
+    })
     return (
-        <header className={styles.header}>
+        <header className={className}>
             <h1>My Site</h1>
             <nav>
                 <ul className={styles.navList}>
@@ -13,10 +23,23 @@ const Header = () => {
                 </ul>
             </nav>
             <div>
-                <button>Switch Theme</button>
+                <button onClick={() => setTheme()}>Switch Theme</button>
             </div>
         </header>
     );
 }
 
-export default Header;
+const mStP = (state) => {
+    return {
+        theme: state.theme,
+        language: state.lang
+    }
+}
+
+const mDtP = (dispatch) => {
+    return {
+        setTheme: () => dispatch(setTheme()),
+    }
+}
+
+export default connect(mStP, mDtP)(Header);
