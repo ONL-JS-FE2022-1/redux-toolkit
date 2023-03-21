72696a51863ff01e863c6bd28ac21ababfb890f1
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-03-21 19:21:34 +0200

    Task here

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>My Site</h1>
+            <nav>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                </ul>
+            </nav>
+            <div>
+                <button>Switch Theme</button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
